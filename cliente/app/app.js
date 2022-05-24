b6c3ac3b44a4f25c7a945572eb3701f450ff2b12
FRisney <41243324+FRisney@users.noreply.github.com>
2022-05-24 02:16:25 -0300

    Atividade WebSockets
    Adicionada funcionalidade de enviar mensagens para conexoes especificas.
    Refatoracao do frontend com tailwind.

--- cliente/app/app.js
+++ cliente/app/app.js
@@ -1,63 +1,116 @@
 const app = Vue.createApp({
-    template: `<div class="chat_window">
-                <div class="top_menu">
-                    <div class="buttons">
-                        <div class="button close"></div>
-                        <div class="button minimize"></div>
-                        <div class="button maximize"></div>
-                    </div>
-                    <div class="title">Chat Programação III</div>
-                </div>
-                <ul class="messages">
-                <div v-for="message in mensagens">
-                <li class="message appeared" :class="message.class" >
-                    <div class="avatar"></div>
-                    <div class="text_wrapper">
-                        <div class="text">
-                            {{message.text}}
-                        </div>
-                    </div>
-                </li>
-                </div>
-                </ul>
-                <div class="bottom_wrapper clearfix">
-                    <div class="message_input_wrapper">
-                        <input v-model="text" class="message_input" placeholder="Escreva sua mensagem aqui..." />
-                    </div>
-                    <div class="send_message">
-                        <div class="icon"></div>
-                        <div v-on:click="send" class="text">Enviar</div>
-                    </div>
-                </div>
+    template: `<div class="bg-slate-700 md:px-28 xl:px-96 md:py-16 xl:py-32 h-screen flex flex-col">
+        <div class="bg-slate-300 rounded-t-xl px-4 py-6 grid grid-cols-3 justify-items-center">
+            <span class="justify-self-start" v-on:click="changeTo">Voltar</span>
+            <div class="text-center text-xl font-bold">Chat Programação III</div>
+            <span class="text-right justify-self-end">{{nome}}</span>
+         </div>
+        <div id="main" class="bg-white grow flex flex-col items-center justify-center">
+            <div v-if="!registerSent" class="flex flex-col gap-8">
+                <input class="grow rounded-md p-4 h-16 border rounded-xl" v-model="nome" type="text" placeholder="digite seu nome">
+                <button class="p-4 rounded-md bg-blue-300" v-on:click="register">Registrar</button>
             </div>
-            <div class="message_template">
-                <li class="message">
-                    <div class="avatar"></div>
-                    <div class="text_wrapper">
-                        <div class="text"></div>
-                    </div>
-                </li>
-            </div>`,
+            <div v-else-if="sel == null" class="flex flex-col gap-8">
+                <select v-model="sel" class="p-4 h-16 w-56 rounded-xl">
+                    <option v-bind:value="contato" v-for="contato in contatos">{{contato}}</option>
+                </select>
+            </div>
+            <div v-else class="grow overflow-scroll flex flex-col self-stretch">
+                <ul>
+                    <li v-for="message in mensagens">
+                        <div v-if="message.type == 'sent'" class="px-4 py-2 flex flex-row items-center justify-end gap-4">
+                            <div id="txt" class="p-4 bg-blue-100 rounded-md">{{message.text}}</div>
+                            <div id="avatar" class="w-12 h-12 bg-blue-600 rounded-full"></div>
+                        </div>
+                        <div v-else class="px-4 py-2 flex flex-row items-center justify-start gap-4">
+                            <div id="avatar" class="w-12 h-12 bg-red-600 rounded-full"></div>
+                            <div id="txt" class="p-4 bg-red-100 rounded-md">{{message.text}}</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="foot" class="bg-slate-200 rounded-b-xl p-4 flex flex-row gap-4 grow-0">
+            <input class="grow rounded-md p-4" v-model="text" type="text" placeholder="Escreva sua mensagem aqui...">
+            <button class="grow-0 rounded-md p-4 bg-green-500 text-white font-bold" v-on:click="send">Enviar</button>
+        </div>
+    </div>`,
     data() {
         return {
             mensagens: [],
+            contatos:[],
             connection: null,
-            text: null
+            text: null,
+            nome:null,
+            sel:null,
+            registerSent: false,
         }
     },
     created() {
         this.connection = new WebSocket('ws://localhost:7777');
         this.listen()
     },
+    computed:{
+        bootstrapped(){
+            let ret = this.sel != null && this.nome != null && this.registerSent;
+            console.log(ret);
+            return ret;
+        },
+        initials(){
+            let arr = this.sel.split(' ');
+            let ret = arr[0].toUpperCase();
+            if (arr.length > 1){
+                ret += arr[1].toUpperCase();
+            }
+            return ret;
+        }
+    },
     methods: {
+        changeTo(){
+            this.sel = null;
+            this.mensagens = [];
+        },
+        register(){
+            let msg = { 'reason':'contacts' };
+            this.connection.send(JSON.stringify(msg));
+            msg = {
+                'reason':'register',
+                'name':this.nome,
+            };
+            console.log(msg);
+            this.connection.send(JSON.stringify(msg));
+            this.registerSent = true;
+        },
         send() {
-            this.connection.send(this.text)
-            this.mensagens.push({'class':'right', 'text': this.text})
-            this.text = ''
+            let msg = {
+                'reason':'dm',
+                'from':this.nome,
+                'to':this.sel,
+                'text':this.text,
+            };
+            console.log(msg);
+            this.connection.send(JSON.stringify(msg));
+            this.mensagens.push({'type':'sent', 'text': this.text});
+            this.text = '';
         },
         listen() {
             this.connection.addEventListener('message', message => {
-                this.mensagens.push({'class':'left', 'text': message.data})
+                let data = JSON.parse(message.data);
+                if (data.hasOwnProperty('reason')){
+                    console.log(data);
+                    switch (data.reason) {
+                        case 'contacts':
+                            this.contatos = data.contatos.filter(e => e !== this.nome);
+                            break;
+                        case 'dm':
+                            console.log(data.text);
+                            if (this.sel !== data.from) break;
+                            this.mensagens.push({'type':'received', 'text': data.text});
+                            break;
+                        default:
+                            console.log('Listen Failed');
+                    }
+                }
             });
         }
     }
--- webchat.php
+++ webchat.php
@@ -15,32 +15,68 @@ $chatComponent = new class implements MessageComponentInterface {
 
     public function __construct()
     {
-        $this->connections = new Set();
+        $this->connections = new Map();
     }
 
-    public function onOpen(ConnectionInterface $conn) 
-    {
-        $this->connections[] = $conn;
-        echo "Quantidade de conexões: " . $this->connections->count() . PHP_EOL;
-    }
+    public function onOpen(ConnectionInterface $conn) {}
+    public function onError(ConnectionInterface $conn, \Exception $e) {}
 
     public function onMessage(ConnectionInterface $from, $msg) 
     {
-        echo "Evento on message" . PHP_EOL;
-        foreach ($this->connections as $conn) {
-            if ($conn !== $from) {
-                $conn->send($msg);
-            }
+        $obj = json_decode($msg);
+        if (!$obj->reason) return;
+        echo $obj->reason;
+        switch ($obj->reason){
+            case 'register': $this->register($from,$obj); break;
+            case 'contacts': $this->sendContacts($from); break;
+            case 'dm': $this->sendDM($obj); break;
         }
     }
 
     public function onClose(ConnectionInterface $conn) 
     {
-        echo "Encerrou a conexão" . PHP_EOL;
+        $key = $this->connections->keys()->get(
+            $this->connections->values()->find($conn)
+        );
+        $this->connections->remove($key);
+
+        echo "$key Encerrou a conexão" . PHP_EOL;
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) 
+    private function register($conn,$msg)
     {
+        echo ' ' . $msg->name . PHP_EOL;
+        $this->connections->put($msg->name, $conn);
+        foreach ($this->connections as $regs){
+            $regs->send(json_encode([
+                'reason'=>'contacts',
+                'contatos'=>$this->connections->keys(),
+            ]));
+        }
+        echo (json_encode($this->connections));
+    }
+
+    private function sendContacts($conn)
+    {
+        echo PHP_EOL;
+        $obj = [
+            'reason'=>'contacts',
+            'contatos'=>$this->connections->keys(),
+        ];
+        $conn->send(json_encode($obj));
+    }
+
+    private function sendDM($msg)
+    {
+        echo ' ' . $msg->text . PHP_EOL;
+        $obj = [
+            'reason'=>'dm',
+            'from' => $msg->from,
+            'text'=>$msg->text,
+        ];
+        $this->connections
+            ->get($msg->to)
+            ->send(json_encode($obj));
     }
 };
 
